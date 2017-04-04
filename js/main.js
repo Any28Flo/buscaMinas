a41c7e309a00aafb0f60c75025ca164adff699cd
“Erika <foglzerika@gmail.com>
2017-04-04 18:20:18 -0500

    Agregando funcion color

--- js/main.js
+++ js/main.js
@@ -1,32 +1,27 @@
-//
 var boton = document.getElementsByClassName("numero");
+
 for (var x=0 ; x<boton.length ; x++){
   boton[x].addEventListener("click",mostrar);
+}
+var bomba= document.getElementsByClassName("bomba");
+for(var y=0 ; y<bomba.length ; y++){
+  bomba[y].addEventListener("click", mostrar_bomba);
 
 }
-var bomba= document.getElementById("bomba");
-bomba.addEventListener("click", mostrar_bomba);
 
+var color = document.getElementsByClassName("color");
+for (var z = 0 ; z < color.length ; z++){
+  color[z].addEventListener("click",cambiaColor);
+}
 function mostrar(){
-  //var valor= document.getElementById("caja");
-  //var x = document.getElementsByClassName("example");
-    //x[0].innerHTML = "Hello World!"
-
-  this.innerText = "1";
+this.innerText = "1";
 }
 
 function mostrar_bomba(){
-
-  var imagen = document.getElementById("bomba");
-  imagen.innerHTML= '<img src="img/imgBomba.jpeg" >'
-
-
-}
-function mostrar_mina() {
-  var img = document.getElementById("mina");
-  img.innerHTML = '<img src= "imagenes/mina.gif"/>';
+  this.innerHTML= '<img src="img/imgBomba.jpeg" >'
 }
 
 function cambiaColor(){
+  this.color.style.backgroundColor
   elemento.style.backgroundColor="#f34";
 }
