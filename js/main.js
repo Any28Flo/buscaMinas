6e25ff2373d52d670adcd1be10f688a2bd1842b1
“Erika <foglzerika@gmail.com>
2017-04-05 10:50:16 -0500

    agregando funcion remueveEventos

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ for (var x=0 ; x<boton.length ; x++){
 }
 
 var bomba= document.getElementsByClassName("bomba");
-for(var y=0 ; y<bomba.length ; y++){
-  bomba[y].addEventListener("click", mostrar_bomba);
 
+  for(var y=0 ; y<bomba.length ; y++){
+    bomba[y].addEventListener("click",mostrar_bomba);
 }
 
 var color = document.getElementsByClassName("color");
@@ -19,12 +19,25 @@ this.innerText = "1";
 }
 
 function mostrar_bomba(){
+  alert("Ya perdiste x.x");
+
   for(var i= 0 ; i< bomba.length ; i++){
     bomba[i].innerHTML= '<img src="img/imgBomba.jpeg" >'
+
   }
+  remueveEventos();
 
 }
 
 function cambiaColor(){
   this.style.backgroundColor="#f34";
+}
+function remueveEventos(){
+
+  for ( var i = 0 ; i< boton.length ; i++ )
+  {
+    //removeEventListener
+    color[i].removeEventListener("click", cambiaColor);
+    boton[i].removeEventListener("click",mostrar);
+  }
 }
